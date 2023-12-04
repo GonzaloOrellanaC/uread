acd2735046d1cc0539bd70c56620d2a5a92ad21e
GonzaloOrellanaC <gonzalo.orellana@kahuel.com>
2023-12-04 00:39:06 -0300

    new server v2

--- client/src/components/Containers/UsoUread/UseUread.container.tsx
+++ client/src/components/Containers/UsoUread/UseUread.container.tsx
@@ -2,13 +2,16 @@ const UseUreadContainer = () => {
     return (
         <div className='content-data-img-bg content-text'>
             <p>
-                ¿Cómo uso URead?  
+                ¿Cómo accedo al contenido Bilingüe? 
             </p>
             <p>
-                1.Regístrate
+                1. Regístrate con tu correo electrónico (solo desde el computador por el momento) 
             </p>
             <p>
-                2.Partir en el Nivel 1. Lectura 1 (no importa el nivel que se tenga)
+                2. Ingresa a tu cuenta y accede a la BIBLIOTECA de lecturas bilingües, audios y material descargable, todo para realizar en casa
+            </p>
+            <p>
+                *No te pierdas las actividades y juntas de lectura en vivo del Club de Lectura Link de Invitación a <a href="https://chat.whatsapp.com/ByLIPtrcgUg6BkRI8m3PvB" target={'_blank'}>URead Reading Club</a>
             </p>
             <p>
                 *Escuchar en grupo y/o individualmente varias veces los audios.  (Se puede escuchar intercambiando  los idiomas de los audios y textos) 
